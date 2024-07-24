b37a0db2ab6c8c8ec6800b0ceb67a3b9695e06b1
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-07-24 10:49:38 -0700

    added code to form.js and made sure everything saves to local storage

---
+++ assets/js/form.js
@@ -0,0 +1,24 @@
+document.getElementById('blogForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const title = document.getElementById('title').value;
+    const content = document.getElementById('content').value;
+  
+    if (!username || !title || !content) {
+      alert('Please complete the form.');
+      return;
+    }
+  
+    const post = {
+      username,
+      title,
+      content
+    };
+  
+    let posts = JSON.parse(localStorage.getItem('posts')) || [];
+    posts.push(post);
+    localStorage.setItem('posts', JSON.stringify(posts));
+  
+    window.location.href = 'blog.html';
+  });
