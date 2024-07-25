3523368cee4d0fbdb374f2abcdf74c19f31a206e
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-07-25 16:44:02 -0700

    added css to the borders for each blog post

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -11,3 +11,22 @@ const toggle = document.getElementById('toggle');
                 darkMode = false
             }
         });
+let blogPosts=document.getElementById("blogPosts")
+  let posts = JSON.parse(localStorage.getItem('posts')) || [];
+  console.log(posts)
+  for (let i = 0; i < posts.length; i++) {
+    let username = posts[i].username
+    let title = posts[i].title
+    let content = posts[i].content
+    let div = document.createElement("div")
+    let h3 = document.createElement("h3")
+    let p1 = document.createElement("p")
+    let p2 = document.createElement("p")
+    h3.textContent=title
+    p1.textContent=content
+    p2.textContent="Posted by: "+ username
+    div.appendChild(h3)
+    div.appendChild(p1)
+    div.appendChild(p2)
+    blogPosts.appendChild(div)
+  }
